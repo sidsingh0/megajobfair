c41a6519698d225ef3eb9966f59f63cca5de22e9
sidsingh0 <sidsinghcs@gmail.com>
2023-08-10 13:40:14 +0530

    cloud id pw

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
     $servername="localhost";
-    $username_db="root";
-    $password="";
-    $database="sof";
+    $username_db="u170697705_sof";
+    $password="Sof@2023";
+    $database="u170697705_sofrootmain";
 
     $conn= mysqli_connect($servername,$username_db,$password,$database);
     if (! $conn ) {
